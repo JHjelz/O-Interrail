a54dc2cc879e080a140d5a4f4fd89beda4e1b10f
jshjelse <jshjelse@stud.ntnu.no>
2025-07-05 21:45:16 +0200

    Fix zoom

--- js/map.js
+++ js/map.js
@@ -1,5 +1,8 @@
 // Opprett kartet
-const map = L.map('map').setView([55.5, 11.5], 4);
+const map = L.map('map', { zoomControl: false }).setView([55.5, 11.5], 4);
+
+// Legger til zoom
+L.control.zoom({ position: 'bottomright' }).addTo(map);
 
 // Legg til bakgrunnskart (OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
