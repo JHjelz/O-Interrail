147255e4a017f4ad6372f95574a11610489b8f6e
jshjelse <jshjelse@stud.ntnu.no>
2025-07-05 17:43:37 +0200

    Clustre punkt

--- js/lastData.js
+++ js/lastData.js
@@ -1,4 +1,4 @@
-// Farger for stasjonstyper
+// Farger for ulike egenskaper (linjer / stasjoner)
 const typeTilFarge = {
     start: "green",
     stasjon: "orange",
@@ -6,44 +6,49 @@ const typeTilFarge = {
     reise: "blue"
 };
 
+const clusterGruppe = L.markerClusterGroup();
+
 // Funksjon: hent ikon basert på type
 function lagIkon(farge) {
     return L.icon({
-      iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-${farge}.png`,
-      shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-      shadowSize: [41, 41]
+        iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-${farge}.png`,
+        shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
     });
-  }
+}
 
 // Last inn GeoJSON
 fetch('reiser.geojson')
   .then(response => response.json())
   .then(data => {
-    L.geoJSON(data, {
-      pointToLayer: function (feature, latlng) {
+    data.features.forEach(feature => {
         const props = feature.properties;
-        const type = props.type || 'stasjon';
-        const farge = typeTilFarge[type] || 'orange';
-        // Automatisk generering av rik popup
-        const popup = `
-            <strong>${props.navn}</strong><br>
-            <table>
-                <tr><td><b>Tid:</b></td><td>${props.tid || '-'}</td></tr>
-                <tr><td><b>Spor:</b></td><td>${props.spor || '-'}</td></tr>
-                <tr><td><b>Land:</b></td><td>${props.land || '-'}</td></tr>
-            </table>
-        `;
-        return L.marker(latlng, { icon: lagIkon(farge) }).bindPopup(popup);
-      },
-
-      onEachFeature: function (feature, layer) {
-        const props = feature.properties;
-        if (feature.geometry.type === 'LineString') {
-            const farge = typeTilFarge(props.type);
-            layer.setStyle({ color: color, weight: 4 });
+        const type = feature.geometry.type;
+        
+        // Punkt
+        if (type == "Point") {
+            const farge = typeTilFarge[props.type] || "orange";
+            // Automatisk generering av rik popup
+            const popup = `
+                <strong>${props.navn}</strong><br>
+                <table>
+                    <tr><td><b>Tid:</b></td><td>${props.tid || '-'}</td></tr>
+                    <tr><td><b>Spor:</b></td><td>${props.spor || '-'}</td></tr>
+                    <tr><td><b>Land:</b></td><td>${props.land || '-'}</td></tr>
+                </table>
+            `;
+            const marker = L.marker(feature.geometry.coordinates.reverse(), {
+                icon: lagIkon(farge)
+            }).bindPopup(popup);
+    
+            clusterGruppe.addLayer(marker);
+        }
+        
+        // Linjer
+        else if (type == 'LineString') {
             const popup = `
                 <strong>${props.navn}</strong><br>
                 <table>
@@ -52,11 +57,17 @@ fetch('reiser.geojson')
                 <tr><td><b>Varighet:</b></td><td>${props.varighet || '-'}</td></tr>
                 </table>
             `;
-          layer.bindPopup(feature.properties.navn);
+            const linje = L.polyline(feature.geometry.coordinates.map(coord => coord.slice().reverse()), {
+                color: typeTilFarge(props.type),
+                weight: 4,
+                opacity: 0.7
+            }).bindPopup(popup);
+            linje.addTo(map)
         }
-      }
-    }).addTo(map);
+    });
+    
+    // Legger til clustering
+    map.addLayer(clusterGruppe);
   })
-  .catch(error => {
-    console.error('Klarte ikke å laste GeoJSON:', error);
-  });
+  .catch(error => console.error('Klarte ikke å laste GeoJSON:', error));
+  
