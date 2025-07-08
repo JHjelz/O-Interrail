c3c04982663b1e10f2ca3940752caae5d09dc104
jshjelse <jshjelse@stud.ntnu.no>
2025-07-08 20:29:02 +0200

    Endre tilbake

--- js/kartKnapper.js
+++ js/kartKnapper.js
@@ -2,71 +2,55 @@
 function lagSidebarLagKontroller() {
     sidebarContent.innerHTML = ""; // Tomt innhold fra start
 
-    const lagSection = (tittel, layers) => {
-        const seksjon = lagDiv("sidebar-section");
+    const lagSektion = (tittel, layers, leggTil, fjern) => {
+        const seksjon = document.createElement("div");
+        seksjon.className = "sidebar-section";
+
         const overskrift = document.createElement("h4");
         overskrift.textContent = tittel;
         seksjon.appendChild(overskrift);
 
         // Master toggle-knapp
-        const masterKnapp = lagKnapp(
-            "Skru av/på alle",
-            "",
-            "master-knapp",
-            fjernAlt(layers)
-        );
+        const masterKnapp = document.createElement("button");
+        masterKnapp.textContent = "Skru av/på alle";
+        masterKnapp.onclick = () => {
+            const påKartet = Object.values(layers)[0]._map !== null;
+            Object.values(layers).forEach(layer => {
+                if (påKartet) fjern(layer); else leggTil(layer);
+            });
+        };
         seksjon.appendChild(masterKnapp);
 
-        // Én knapp per element
+        // En knapp per element
         for (const [id, lag] of Object.entries(layers)) {
-            const knapp = lagKnapp(
-                id,
-                "",
-                "lag-knapp",
-                fjernEttLag(lag)
-            );
+            const knapp = document.createElement("button");
+            knapp.textContent = id;
+            knapp.className = "lag-knapp";
+            knapp.onclick = () => {
+                const påKartet = lag._map !== null;
+                if (påKartet) {
+                    fjern(lag);
+                    knapp.classList.add("av");
+                } else {
+                    leggTil(lag);
+                    knapp.classList.remove("av");
+                }
+            };
             seksjon.appendChild(knapp);
         }
+
         sidebarContent.appendChild(seksjon);
     };
 
     // Lag linje-seksjon
-    lagSection("Linjer", lineLayers);
+    lagSektion("Linjer", lineLayers,
+        lag => lag.addTo(map),
+        lag => map.removeLayer(lag)
+    );
 
     // Lag punkt-seksjon (via cluster)
-    lagSection("Steder", markerLayers);
-}
-
-// Hjelpefunksjoner
-function leggTil(lag) {
-    lag.addTo(map);
-}
-function fjern(lag) {
-    map.removeLayer(lag);
-}
-
-// Funksjon som fjerner alle kartlag
-function fjernAlt(layers) {
-    const påKartet = Object.values(layers)[0]._map !== null;
-    Object.values(layers).forEach(layer => {
-        if (påKartet) {
-            fjern(layer);
-            masterKnapp.classList.add("av");
-            } else {
-            leggTil(layer);
-            masterKnapp.classList.remove("av");
-            }
-    });
-}
-
-// Funksjon som fjerner ett kartlag
-function fjernEttLag(lag) {
-    const påKartet = lag._map !== null;
-    if (påKartet) {
-        fjern(lag);
-        knapp.classList.add("av");
-    } else {
-        leggTil(lag);
-        knapp.classList.remove("av");
-    }
+    lagSektion("Steder", markerLayers,
+        lag => clusterGruppe.addLayer(lag),
+        lag => clusterGruppe.removeLayer(lag)
+    );
 }
