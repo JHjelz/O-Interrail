07ac4fdbf52e41e097e5f6dad63d299bb43c94e9
jshjelse <jshjelse@stud.ntnu.no>
2025-08-23 22:05:59 +0200

    Fix

--- js/introTrains.js
+++ js/introTrains.js
@@ -1,35 +1,37 @@
 // O-INTERRAIL/js/introTrains.js
 
-const intro = document.getElementById("intro");
-const content = document.getElementById("content");
-const flagsContainer = document.getElementById("flags");
+document.addEventListener("DOMContentLoaded", () =>{
+    const intro = document.getElementById("intro");
+    const content = document.getElementById("content");
+    const flagsContainer = document.getElementById("flags");
 
-function createFlags() {
-    for (let i=0; i<10; i++) {
-        const flag = document.createElement("img");
-        flag.src = "Orienteringspost.png";
-        flag.classList.add("flag");
-        flag.style.left = Math.random() * 90 + "vw";
-        flag.style.animationDelay = (Math.random() * 2) + "s";
-        flagsContainer.appendChild(flag);
-    }
-}
-
-createFlags();
-
-const trains = document.querySelectorAll(".train");
-let finished = 0;
-
-trains.forEach(train => {
-    train.addEventListener("animationend", () => {
-        finished++;
-        if (finished == trains.length) {
-            intro.style.transition = "opacity 0.8s";
-            intro.style.opacity = 0;
-            setTimeout(() => {
-                intro.style.display = "none";
-                content.style.display = "block";
-            }, 1000);
+    function createFlags() {
+        for (let i=0; i<10; i++) {
+            const flag = document.createElement("img");
+            flag.src = "Orienteringspost.png";
+            flag.classList.add("flag");
+            flag.style.left = Math.random() * 90 + "vw";
+            flag.style.animationDelay = (Math.random() * 2) + "s";
+            flagsContainer.appendChild(flag);
         }
+    }
+
+    createFlags();
+
+    const trains = document.querySelectorAll(".train");
+    let finished = 0;
+
+    trains.forEach(train => {
+        train.addEventListener("animationend", () => {
+            finished++;
+            if (finished == trains.length) {
+                intro.style.transition = "opacity 0.8s";
+                intro.style.opacity = 0;
+                setTimeout(() => {
+                    intro.style.display = "none";
+                    content.style.display = "block";
+                }, 1000);
+            }
+        });
     });
 });
